58d046d3f1219aa055bd3a275d7e7cf0ded9ce0c
Thomas Liske <thomas@fiasko-nw.net>
2012-03-22 20:06:53 +0100

    Begin configuration build script.

---
+++ lib/mkconfig.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+XSLDIR=${2:-/usr/share/scotty-rel/xsl}
+CFGDIR=${1:-/etc/scotty-rel}
+WWWDIR=${2:-/var/www/scotty-rel}
+VIEWSDIR="$WWWDIR/views"
+
+function err {
+    logger -s -t scotty-rel -- "Configuration error: $@"
+    exit 1
+}
+
+SRVFN=`tempfile -p srv -s xml`
+V2SFN=`tempfile -p v2s -s xml`
+for fn in "$VIEWSDIR/"*.svg; do
+    if [ ! -e "$fn" ]; then
+	rm "$SRVFN"
+	rm "$V2SFN"
+	err no view found "($VIEWSDIR/*.svg)"
+    fi
+
+    xsltproc -o "$V2SFN" "$XSLDIR/view2services.xsl" "$fn" || err xsltproc processing failed on $fn
+    tail -n +2 "$V2SFN" >> "$SRVFN"
+done
+rm "$V2SFN"
+
+cat "$SRVFN"
+
+rm "$SRVFN"
